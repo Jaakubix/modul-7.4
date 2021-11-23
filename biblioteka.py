6b1e751717882b6f19d8d0ffb21adcc3bc4b94a7
Jakub Piskorz <kpiskorz13@gmail.com>
2021-11-23 14:03:17 +0100

    create classes film and serial

---
+++ biblioteka.py
@@ -0,0 +1,20 @@
+class film:
+    def __init__(self,tytul,rok_wydania,gatunek,liczba_odtworzen):
+        self.tytul = tytul
+        self.rok_wydania = rok_wydania
+        self.gatunek = gatunek
+        self.liczba_odtworzen = liczba_odtworzen
+        self.liczba_odtworzen = 0
+        def play(self, odtw = 1)
+            self.liczba_odtworzen += odtw
+class serial:
+    def __init__(self,tytul,rok_wydania,gatunek,sezon,odcinek,liczba_odtworzen):
+        self.tytul = tytul
+        self.rok_wydania = rok_wydania
+        self.gatunek = gatunek
+        self.sezon = sezon
+        self.odcinek = odcinek
+        self.liczba_odtworzen = liczba_odtworzen
+        self.liczba_odtworzen = 0
+        def play(self, odtw = 1)
+            self.liczba_odtworzen += odtw
